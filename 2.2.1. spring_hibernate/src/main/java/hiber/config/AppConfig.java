7fdbe37bcf8d48a3c313a1548049b5e4c6e3ff35
Sleepforadream <maxnervannn@gmail.com>
2023-05-02 17:24:40 +0500

    update 02.05.2023

--- 2.2.1. spring_hibernate/src/main/java/hiber/config/AppConfig.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/config/AppConfig.java
@@ -23,9 +23,11 @@ import java.util.Properties;
 @EnableTransactionManagement
 @ComponentScan(value = "hiber")
 public class AppConfig {
-
-    @Autowired
     private Environment env;
+    @Autowired
+    void setEnviroment(Environment env){
+        this.env = env;
+    }
 
    @Bean
    public DataSource getDataSource() {
